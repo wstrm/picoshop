66f08d0c6de04240897ab0e1d281cad82c03f040
naddah-5 <naddah-5@student.ltu.se>
2017-12-11 14:56:49 +0100

    .

--- model/init.sql
+++ model/init.sql
@@ -121,7 +121,7 @@ CREATE TABLE IF NOT EXISTS `order` (
 	id INT AUTO_INCREMENT,
 	customer INT NOT NULL,
 	address INT NOT NULL,
-	status INT(3) NOT NULL, -- status, future proof with 3 long int
+	status BIT(2) NOT NULL, -- status, future proof designated by two bits
 	create_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP);
 
 /* Create user_has_orders table */
