41c7cbb27abb059bb8ff12c39b8bc7a1011e71db
naddah-5 <naddah-5@student.ltu.se>
2017-12-13 11:00:30 +0100

    Connected warehouse order view and control to DB

--- controller/warehouse/warehouse.go
+++ controller/warehouse/warehouse.go
@@ -58,27 +58,3 @@ func (a *warehouseHandler) ServeHTTP(writer http.ResponseWriter, request *http.R
 func NewHandler() *warehouseHandler {
 	return &warehouseHandler{}
 }
-
-func (c *cartHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	switch request.Method {
-	case http.MethodGet: // View cart
-		view.Render(request.Context(), writer, "cart", view.Page{Title: "Picoshop"})
-
-	case http.MethodPost: // Add article to cart
-		http.Error(writer, "", http.StatusNotImplemented)
-
-	case http.MethodPut: // Order items in cart
-		http.Error(writer, "", http.StatusNotImplemented)
-
-	case http.MethodDelete:
-		pos := request.URL.Query().Get("pos")
-		if pos != "" {
-			// Delete specific item
-		} else {
-			// Delete whole cart
-		}
-
-	default:
-		http.Error(writer, "", http.StatusMethodNotAllowed)
-	}
-}
